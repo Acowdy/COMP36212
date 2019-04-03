2ea291e3d2b6d31987f38873a8da14428849cd7c
Adam Cowdy <cowdyadam@gmail.com>
2019-04-03 11:45:27 +0100

    Improve methods.py

--- methods.py
+++ methods.py
@@ -1,10 +1,12 @@
-def forward_euler(f, x0, xn, y0, n):
+import math
+
+def forward_euler(f, x0, xn, y0, h):
     """
     Solve an IVP y' = f(x, y) using the forward Euler method where
     x0 is the initial value of x, xn is the final value of x, y0 is the
-    initial value of y and n is the number of steps.
+    initial value of y and h is the step size.
     """
-    h = (xn - x0) / n
+    n = math.floor((xn - x0) / h)
     x = x0
     y = y0
     for _ in range(n):
@@ -13,13 +15,13 @@ def forward_euler(f, x0, xn, y0, n):
     return y
 
 
-def ab2(f, x0, xn, y0, n):
+def ab2(f, x0, xn, y0, h):
     """
     Solve an IVP y' = f(x, y) using the AB2 method where
     x0 is the initial value of x, xn is the final value of x, y0 is the
-    initial value of y and n is the number of steps.
+    initial value of y and h is the step size.
     """
-    h = (xn - x0) / n
+    n = math.floor((xn - x0) / h)
     x1 = x0 + h
     y1 = forward_euler(f, x0, x1, y0, 1)
     for _ in range(n-1):
@@ -33,5 +35,5 @@ def ab2(f, x0, xn, y0, n):
 
 
 if __name__ == "__main__":
-    print(forward_euler(lambda x, y: y, 0, 4, 1, 4))
-    print(ab2(lambda x, y: y, 0, 2, 1, 4))
+    print(forward_euler(lambda x, y: y, 0, 4, 1, 1))
+    print(ab2(lambda x, y: y, 0, 2, 1, 0.5))
